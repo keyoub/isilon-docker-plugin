1e70a3876c0946a4511dc44eaf98de5095a749c8
bkeyoumarsi <bkeyoumarsi@isilon.com>
2015-07-23 16:45:04 -0700

    I think its done for ShipItDay :)

--- main.go
+++ main.go
@@ -1,32 +1,56 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/calavera/dkvolume"
 	"github.west.isilon.com/bkeyoumarsi/docker-plugin/driver"
 )
 
-const socketAddress = "/usr/share/docker/plugins/isilon.sock"
+const (
+	socketAddress = "/usr/share/docker/plugins/isilon.sock"
+	mountPoint    = "/tmp/isilon/volumes/"
+)
 
 func main() {
-	var clusterPath string
+	var (
+		clusterIP string
+	)
 
-	flag.StringVar(&clusterPath, "path", "",
-		"Local directory path where Isilon cluster is mounted on")
+	flag.StringVar(&clusterIP, "cluster-ip", "",
+		"Isilon cluster ip in form of <X.X.X.X>")
 
 	flag.Parse()
 
-	if _, err := os.Stat(clusterPath); os.IsNotExist(err) {
-		fmt.Printf("no such file or directory: %s\n", clusterPath)
-		os.Exit(1)
+	err := mountCluster(clusterIP)
+	if err != nil {
+		log.Panic(err.Error())
 	}
 
-	d := driver.NewIsilonDriver(clusterPath)
+	d := driver.NewIsilonDriver()
 	handler := dkvolume.NewHandler(d)
 	log.Printf("listening on %s\n", socketAddress)
 	log.Fatal(handler.ServeUnix("root", socketAddress))
+}
+
+func mountCluster(clusterIP string) error {
+	if _, err := os.Stat(mountPoint); os.IsNotExist(err) {
+		log.Println("Creating temp mount point")
+		if err := os.MkdirAll(mountPoint, 0755); err != nil {
+			return errors.New("Failed to create mountpoint directory")
+		}
+		mountPath := fmt.Sprintf("%s:/ifs/data/docker/volumes", clusterIP)
+		cmd := exec.Command("mount", "-t", "nfs", mountPath, mountPoint)
+
+		err := cmd.Run()
+		if err != nil {
+			return errors.New("Failed to mount Isilon cluster")
+		}
+	}
+	return nil
 }
